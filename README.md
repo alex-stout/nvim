102419beb3f256943e6c6f41343b7a1cb86d53d6
alex-stout <17804548+alex-stout@users.noreply.github.com>
2025-09-23 20:41:07 -0600

    feat: add markown rendering

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Nvim config
 
 My NVIM config.
+
+Designed for simplicity. Plugins only added if frequently used.
---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,13 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+    config = function()
+        local markdwon = require('render-markdown')
+        markdwon.setup({
+            completions = { lsp = { enabled = true } },
+        })
+    end
+}
